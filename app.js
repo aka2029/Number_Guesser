46eabde340fb99af72a72b6200f5ee86112ce723
aka2029 <asaks22@gmail.com>
2020-05-10 15:39:16 +0530

    new commit

--- app.js
+++ app.js
@@ -25,6 +25,13 @@ const game = document.querySelector("#game"),
 minNum.textContent = min;
 maxNum.textContent = max;
 
+// Play again event listener
+game.addEventListener("mousedown", function(e) {
+  if (e.target.className === "play-again") {
+    window.location.reload();
+  }
+});
+
 // Listen for guess
 guessBtn.addEventListener("click", function() {
   let guess = parseInt(guessInput.value);
@@ -79,6 +86,10 @@ function gameOver(won, msg) {
   message.style.color = color;
   // Set Message
   setMessage(msg);
+
+  // Play Again
+  guessBtn.value = "Play Again";
+  guessBtn.className += "play-again"; //here we are appending ie if it already has a class the we'll add this with it
 }
 
 //set message
