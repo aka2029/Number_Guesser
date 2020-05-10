34360bdab6cdde43deb596ae924d33db38866119
aka2029 <asaks22@gmail.com>
2020-05-10 13:59:52 +0530

    new commit

---
+++ app.js
@@ -0,0 +1,10 @@
+/*
+GAME FUNCTION:
+- Player must guess a number between a min & max
+- Player gets a certain amount of guesses
+- Notify player of guessses remaining
+- Notify the player of the correct answer if loose
+- Let player choose to play again
+ */
+
+//  Game values
